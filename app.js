613128c3a26b592ed76c8abc1222189de06a8512
Gregorio Juliana <gregojquiros@gmail.com>
2016-05-17 22:13:29 +0200

    First working release

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const Vorpal = require('vorpal');
-const chalk = Vorpal().chalk;
-const loadtester = new Vorpal();
+const vorpal = require('vorpal');
+const chalk = vorpal().chalk;
+const loadtester = new vorpal();
 const http = require('http');
 const async = require('async');
 const fs = require('fs');
 const moniker = require('moniker');
+const progressBar = require('progress');
+var https = require('https');
 
 function request(options, callback) {
 	const result = {};
@@ -23,34 +25,77 @@ function request(options, callback) {
 	req.end();
 }
 
+function processResults(options, results) {
+	var toPrint = {
+		totalRequests: results.length,
+		totalAverage: 0,
+		totalErrors: 0,
+		clientStats: {}
+	};
+	results.forEach(function (result) {
+		toPrint.totalAverage += result.endTime - result.initTime;
+		let clientStats = toPrint.clientStats[result.client];
+		if (!clientStats) {
+			clientStats = {
+				requests: 0,
+				average: 0,
+				errors: 0
+			}
+			toPrint.clientStats[result.client] = clientStats;
+		}
+		clientStats.requests++;
+		clientStats.average += result.endTime - result.initTime;
+		if (result.error) {
+			toPrint.totalErrors++;
+			clienStats.errors++;
+		}
+	});
+	toPrint.totalAverage /= toPrint.totalRequests;
+	for (var clientName in toPrint.clientStats) {
+		toPrint.clientStats[clientName].average /= toPrint.clientStats[clientName].requests
+	}
+	if (!options.verbose)
+		delete toPrint.clientStats;
+	return toPrint;
+}
+
 
 loadtester.command('fire <url>')
-	.description('Sends requests to the specified URL, logging the time')
+	.description('Sends requests to the specified URL, logging the time and errors')
 	.option('-m --method <method>', 'HTTP Method')
 	.option('-p --port <port>', 'Port, if other than 80 or 443')
-	.option('-c --clients <clients>', 'Number of simultaneous clients')
-	.option('-r --requests <requests>', 'Max. number of requests per client')
-	.option('-d --duration <duration>', 'Max. duration of the test in seconds')
-	.option('-f, --file <path>', 'Output file')
+	.option('-c --clients <clients>', 'Number of simultaneous clients. Defaults to 1.')
+	.option('-r --requests <requests>', 'Max. number of requests per client. Defaults to 10.')
+	.option('-d --duration <duration>', 'Max. duration of the test in seconds. Defaults to 10')
+	.option('-f --file <path>', 'Output file')
+	.option('-w --raw', 'Store raw results')
+	.option('-v --verbose', 'Detailed results')
 	.action((args, callback) => {
+		const options = args.options;
 		const url = args.url.match(new RegExp("^(.*:)//([A-Za-z0-9\-\.]+)(:[0-9]+)?(.*)$"));
 		const httpOptions = {
 			host: url[2] + (url[3] || ""),
 			path: url[4] || "/"
 		}
-		if (args.options.method)
-			httpOptions.method = args.options.method
-		if (args.options.port)
-			httpOptions.port = args.options.port
-		else if(url[1] == "http:")
+		if (options.method)
+			httpOptions.method = options.method
+		if (options.port)
+			httpOptions.port = options.port
+		else if (url[1] == "http:")
 			httpOptions.port = 80;
-		else 
+		else
 			httpOptions.port = 443;
-		const parallelRequests = args.options.clients || 1;
-		const maxRequestsPerClient = args.options.requests || 10;
-		let remainingTime = args.options.duration || 10;
-		const options = {};
+		const parallelRequests = options.clients || 1;
+		const maxRequestsPerClient = options.requests || 10;
+		let remainingTime = options.duration || 10;
 		options.http = httpOptions;
+		loadtester.log("");
+		var bar = new progressBar('Firing tiny hamsters against ${url} [:bar] :percent :etas', {
+			complete: '=',
+			incomplete: ' ',
+			width: 50,
+			total: parallelRequests * maxRequestsPerClient
+		});
 		function wrapClient(callback) {
 			let requests = Number(maxRequestsPerClient);
 			const results = [];
@@ -60,6 +105,7 @@ loadtester.command('fire <url>')
 					requests--;
 					result.client = name;
 					results.push(result);
+					bar.tick();
 					return callback(undefined);
 				});
 			}, (err) => {
@@ -72,9 +118,17 @@ loadtester.command('fire <url>')
 			});
 		}, (err, results) => {
 			results = [].concat.apply([], results);
-			if(args.options.file)
-				fs.writeFileSync(args.options.file, JSON.stringify(results, null, 4));
-			loadtester.log(results);
+			let processedResults = processResults(options, results);
+			loadtester.log("");
+			loadtester.log(JSON.stringify(processedResults, null, 4));
+			loadtester.log("");
+			if (options.file) {
+				if (options.raw)
+					fs.writeFileSync(options.file, JSON.stringify(results, null, 4));
+				else
+					fs.writeFileSync(options.file, JSON.stringify(processedResults, null, 4));
+			}
+
 			return callback();
 		});
 		let tick = function () {
@@ -86,4 +140,4 @@ loadtester.command('fire <url>')
 		tick();
 	});
 
-loadtester.delimiter(chalk.cyan('loadtester~$')).show();
+loadtester.delimiter('loadtester~$').show();
