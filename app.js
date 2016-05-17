e0bd9aa034be11199dac01c8fc5df31d1eed134f
Gregorio Juliana <gregojquiros@gmail.com>
2016-05-17 16:31:15 +0200

    Almost completed first version

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ const async = require('async');
 const fs = require('fs');
 const moniker = require('moniker');
 
-const request = function(httpOptions, options, callback){
+function request(options, callback) {
 	const result = {};
-	const req = http.request(httpOptions, (res) => {
+	const req = http.request(options.http, (res) => {
 		result.endTime = Date.now();
 		return callback(undefined, result);
 	});
 
 	req.on('error', (err) => {
 		result.endTime = Date.now();
-		result.error = err;
+		result.error = err.message;
 		return callback(undefined, result)
 	});
 
@@ -26,53 +26,59 @@ const request = function(httpOptions, options, callback){
 
 loadtester.command('fire <url>')
 	.description('Sends requests to the specified URL, logging the time')
-	.option('-m --method', 'HTTP Method')
-	.option('-p --port', 'Port, if other than 80 or 443')
-	.option('-c --clients', 'Number of simultaneous clients')
-	.option('-r --requests', 'Max. number of requests per client')
-	.option('-d --duration', 'Max. duration of the test in seconds')
-	.option('-f, --file', 'Output file')
-	.option('-s, --save-body', 'Save response body').hidden()
-	.action((args, callback) => {{}
+	.option('-m --method <method>', 'HTTP Method')
+	.option('-p --port <port>', 'Port, if other than 80 or 443')
+	.option('-c --clients <clients>', 'Number of simultaneous clients')
+	.option('-r --requests <requests>', 'Max. number of requests per client')
+	.option('-d --duration <duration>', 'Max. duration of the test in seconds')
+	.option('-f, --file <path>', 'Output file')
+	.action((args, callback) => {
+		const url = args.url.match(new RegExp("^(.*:)//([A-Za-z0-9\-\.]+)(:[0-9]+)?(.*)$"));
 		const httpOptions = {
-			host: args.url.substring(args.url.indexOf('/') + 1, args.url.length),
-			path: args.url.substring(0, args.url.indexOf('/'))
+			host: url[2] + (url[3] || ""),
+			path: url[4] || "/"
 		}
-		const options = {}
-		if(args.options.method)
+		if (args.options.method)
 			httpOptions.method = args.options.method
-		if(args.options.port)
-			httpOptions.port = args.options.port 
+		if (args.options.port)
+			httpOptions.port = args.options.port
+		else if(url[1] == "http:")
+			httpOptions.port = 80;
+		else 
+			httpOptions.port = 443;
 		const parallelRequests = args.options.clients || 1;
-		const maxRequestsPerClient = args.options.requests || 10000000;
+		const maxRequestsPerClient = args.options.requests || 10;
 		let remainingTime = args.options.duration || 10;
-		let executeClient = function(httpOptions, options, callback){
+		const options = {};
+		options.http = httpOptions;
+		function wrapClient(callback) {
 			let requests = Number(maxRequestsPerClient);
 			const results = [];
 			const name = moniker.choose();
-			async.doWhilst(() => { return remainingTime > 0 && requests > 0 }, (callback) => {
-				request(httpOptions, options, (err, result) => {
+			async.whilst(() => { return remainingTime > 0 && requests > 0 }, (callback) => {
+				request(options, (err, result) => {
 					requests--;
-					result.client = name; 
+					result.client = name;
 					results.push(result);
 					return callback(undefined);
 				});
-			}, 	
-			(err) => {
-				this.log("Client ${name} generated: ${results}");
+			}, (err) => {
 				return callback(undefined, results);
 			});
 		}
 		async.times(parallelRequests, (n, next) => {
-			executeClient(httpOptions, args.options, (err, results)=>{
+			wrapClient((err, results) => {
 				next(err, results);
 			});
-		}, 
-		(err, results) => {
-			this.log(results);
+		}, (err, results) => {
+			results = [].concat.apply([], results);
+			if(args.options.file)
+				fs.writeFileSync(args.options.file, JSON.stringify(results, null, 4));
+			loadtester.log(results);
+			return callback();
 		});
-		let tick = function(){
-			if(remainingTime > 0){
+		let tick = function () {
+			if (remainingTime > 0) {
 				remainingTime--;
 				setTimeout(tick, 1000);
 			}
